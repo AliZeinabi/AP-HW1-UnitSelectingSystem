b0526bed1d5b77e370f7599576eb65042ecad3ff
Ali <Ali@Ali.com>
2024-03-07 13:06:50 +0330

    Bug fixed: change capacity for admin

--- src/courses/ShowCourse.java
+++ src/courses/ShowCourse.java
@@ -1,52 +1,48 @@
 package courses;
 public abstract class ShowCourse {
     public void showMath() {
-        CourseList CourseList1 = new CourseList();
-        for (generalcourse course: CourseList1.getGenerallist().values()) {
+        for (generalcourse course: CourseList.getGenerallist().values()) {
             if (course.getDepartment().equals("Mathematics")){
                 System.out.println("Course Code:"+course.getCode()+"*Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule:"+course.getday()+">"+course.getHour()+"*Type:general"+"\n");
             }
         }
-        for (propercourse course: CourseList1.getProperlist().values()) {
+        for (propercourse course: CourseList.getProperlist().values()) {
             if (course.getDepartment().equals("Mathematics")){
                 System.out.println("Course Code:"+course.getCode()+"*Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule:"+course.getday()+">"+course.getHour()+"*Type:Notgeneral"+"\n");
             }
         }
     }
     public void showCE() {
-        CourseList C = new CourseList();
-        for (generalcourse course: C.getGenerallist().values()) {
+        for (generalcourse course: CourseList.getGenerallist().values()) {
             if (course.getDepartment().equals("Computer engineering")){
                 System.out.println("Course Code:"+course.getCode()+"*Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule:"+course.getday()+">"+course.getHour()+"Type:General");
             }
         }
-        for (propercourse course: C.getProperlist().values()) {
+        for (propercourse course: CourseList.getProperlist().values()) {
             if (course.getDepartment().equals("Computer engineering")){
                 System.out.println("Course Code:"+course.getCode()+"*Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule:"+course.getday()+">"+course.getHour()+"*Type:Notgeneral");
             }
         }
     }
     public void showLang() {
-        CourseList L = new CourseList();
-        for (generalcourse course: L.getGenerallist().values()) {
+        for (generalcourse course: CourseList.getGenerallist().values()) {
             if (course.getDepartment().equals("Language Center")){
                 System.out.println("Course Code:"+course.getCode()+"*Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule: "+course.getday()+">"+course.getHour()+"Type:general");
             }
         }
-        for (propercourse course: L.getProperlist().values()) {
+        for (propercourse course: CourseList.getProperlist().values()) {
             if (course.getDepartment().equals("Language Center")){
                 System.out.println("Course Code:"+course.getCode()+" *Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule:"+course.getday()+">"+course.getHour()+"*Type:Notgeneral");
             }
         }
     }
     public void showPhysic() {
-        CourseList P = new CourseList();
-        for (generalcourse course: P.getGenerallist().values()) {
+        for (generalcourse course: CourseList.getGenerallist().values()) {
             if (course.getDepartment().equals("Physics")){
                 System.out.println("Course Code: "+course.getCode()+"*Unit worth: "+course.getWorth()+"*Course: "+course.getName()+"*Capacity: "+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam: "+course.getExamdate()+"*weekly Schedule: "+course.getday()+" "+course.getHour()+"Type:general");
             }
         }
-        for (propercourse course: P.getProperlist().values()) {
+        for (propercourse course: CourseList.getProperlist().values()) {
             if (course.getDepartment().equals("Physics")){
                 System.out.println("Course Code:"+course.getCode()+"*Unit worth:"+course.getWorth()+"*Course:"+course.getName()+"*Capacity:"+course.getCapacity()+"*Instructor:"+course.getInstructor()+"*Date of Final Exam:"+course.getExamdate()+"*weekly Schedule:"+course.getday()+">"+course.getHour()+"*Type:Notgeneral");
             }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,11 @@
 import ConnectToUser.cli;
 import courses.*;
 import ConnectToUser.cli;
+
 public class main {
     public static void main(String[] args) {
-        cli cli = new cli();
-        cli.start();
+        InitialCourseList.InitialCourseList();
+        ConnectToUser.cli.start();
+
     }
 }
