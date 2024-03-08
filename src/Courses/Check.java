d1629b7dec477949581504642a5c04b3737b2a7c
Ali <Ali@Ali.com>
2024-03-08 11:53:49 +0330

    merge  abstract branch to debug branch!some commits deleted :')

--- src/Courses/Check.java
+++ src/Courses/Check.java
@@ -22,61 +22,99 @@ public class Check {
         }
         boolean run = false;
         boolean isoverlapday = false;
-        for (GeneralCourse course:StudentData.usergencourselist.get(StudentData.currentuser.getstudentnumber()).values()){
-            for (String day1:course.getday().split("-")){
-                for (String day2:CourseList.getGenerallist().get(coursecode).getday().split("-")){
-                    if (day1.equals(day2)){
-                        isoverlapday=true;
-                        run = true;
-                        break;
+        try {
+            for (GeneralCourse course:StudentData.usergencourselist.get(StudentData.currentuser.getstudentnumber()).values()){
+                for (String day1:course.getday().split("-")){
+                    for (String day2:CourseList.getGenerallist().get(coursecode).getday().split("-")){
+                        if (day1.equals(day2)){
+                            isoverlapday=true;
+                            run = true;
+                            break;
+                        }
                     }
+                    if (run){break;}
                 }
                 if (run){break;}
             }
-            if (run){break;}
-        }
-        for (ProperCourse course:StudentData.userprocourselist.get(StudentData.currentuser.getstudentnumber()).values()){
-            for (String day1:course.getday().split("-")){
-                for (String day2:CourseList.getProperlist().get(coursecode).getday().split("-")){
-                    if (day1.equals(day2)){
-                        isoverlapday=true;
-                        run = true;
-                        break;
+
+        }catch (Exception e){
+            for (ProperCourse course:StudentData.userprocourselist.get(StudentData.currentuser.getstudentnumber()).values()){
+                for (String day1:course.getday().split("-")){
+                    for (String day2:CourseList.getProperlist().get(coursecode).getday().split("-")){
+                        if (day1.equals(day2)){
+                            isoverlapday=true;
+                            run = true;
+                            break;
+                        }
                     }
+                    if (run){break;}
                 }
                 if (run){break;}
             }
-            if (run){break;}
         }
         isoverlap=false;
-        if (isoverlapday){
-            for (GeneralCourse course:StudentData.usergencourselist.get(StudentData.currentuser.getstudentnumber()).values()){
-                int coursestart = Integer.parseInt(course.getHour().split("-")[0].split(":")[0]);
-                int courseend = Integer.parseInt(course.getHour().split("-")[1].split(":")[1]);
-                int coursestartmin = Integer.parseInt(course.getHour().split("-")[0].split(":")[1]);
-                int courseendmin = Integer.parseInt(course.getHour().split("-")[1].split(":")[1]);
-                int newcoursestart = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[0].split(":")[0]);
-                int newcoursestartmin = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[0].split(":")[1]);
-                int newcourseend = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[1].split(":")[0]);
-                int newcourseendmin = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[1].split(":")[1]);
-                if (coursestart < newcoursestart && newcoursestart< courseend){
-                    isoverlap=true;
-                } else if (coursestart==newcoursestart && newcoursestartmin>coursestartmin ) {
-                    isoverlap=true;
-                } else if (newcoursestart==courseend && newcoursestartmin<courseendmin ) {
-                    isoverlap=true;
-                } else if (coursestart < newcourseend && newcoursestart< courseend){
-                    isoverlap=true;
-                } else if (coursestart==newcourseend && newcourseendmin>coursestartmin ) {
-                    isoverlap=true;
-                } else if (newcourseend==courseend && newcourseendmin<courseendmin ) {
-                    isoverlap=true;
-                } else if (newcoursestart==coursestart && newcoursestartmin==coursestartmin) {
-                    isoverlap=true;
-                } else if (newcourseend==courseend && courseendmin==newcourseendmin) {
-                    isoverlap=true;
+        try {
+            if (isoverlapday){
+                for (GeneralCourse course:StudentData.usergencourselist.get(StudentData.currentuser.getstudentnumber()).values()){
+                    int coursestart = Integer.parseInt(course.getHour().split("-")[0].split(":")[0]);
+                    int courseend = Integer.parseInt(course.getHour().split("-")[1].split(":")[1]);
+                    int coursestartmin = Integer.parseInt(course.getHour().split("-")[0].split(":")[1]);
+                    int courseendmin = Integer.parseInt(course.getHour().split("-")[1].split(":")[1]);
+                    int newcoursestart = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[0].split(":")[0]);
+                    int newcoursestartmin = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[0].split(":")[1]);
+                    int newcourseend = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[1].split(":")[0]);
+                    int newcourseendmin = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[1].split(":")[1]);
+                    if (coursestart < newcoursestart && newcoursestart< courseend){
+                        isoverlap=true;
+                    } else if (coursestart==newcoursestart && newcoursestartmin>coursestartmin ) {
+                        isoverlap=true;
+                    } else if (newcoursestart==courseend && newcoursestartmin<courseendmin ) {
+                        isoverlap=true;
+                    } else if (coursestart < newcourseend && newcoursestart< courseend){
+                        isoverlap=true;
+                    } else if (coursestart==newcourseend && newcourseendmin>coursestartmin ) {
+                        isoverlap=true;
+                    } else if (newcourseend==courseend && newcourseendmin<courseendmin ) {
+                        isoverlap=true;
+                    } else if (newcoursestart==coursestart && newcoursestartmin==coursestartmin) {
+                        isoverlap=true;
+                    } else if (newcourseend==courseend && courseendmin==newcourseendmin) {
+                        isoverlap=true;
+                    }
                 }
             }
+
+        }catch (Exception e){
+            if (isoverlapday){
+                for (ProperCourse course:StudentData.userprocourselist.get(StudentData.currentuser.getstudentnumber()).values()){
+                    int coursestart = Integer.parseInt(course.getHour().split("-")[0].split(":")[0]);
+                    int courseend = Integer.parseInt(course.getHour().split("-")[1].split(":")[1]);
+                    int coursestartmin = Integer.parseInt(course.getHour().split("-")[0].split(":")[1]);
+                    int courseendmin = Integer.parseInt(course.getHour().split("-")[1].split(":")[1]);
+                    int newcoursestart = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[0].split(":")[0]);
+                    int newcoursestartmin = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[0].split(":")[1]);
+                    int newcourseend = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[1].split(":")[0]);
+                    int newcourseendmin = Integer.parseInt(CourseList.getGenerallist().get(coursecode).getHour().split("-")[1].split(":")[1]);
+                    if (coursestart < newcoursestart && newcoursestart< courseend){
+                        isoverlap=true;
+                    } else if (coursestart==newcoursestart && newcoursestartmin>coursestartmin ) {
+                        isoverlap=true;
+                    } else if (newcoursestart==courseend && newcoursestartmin<courseendmin ) {
+                        isoverlap=true;
+                    } else if (coursestart < newcourseend && newcoursestart< courseend){
+                        isoverlap=true;
+                    } else if (coursestart==newcourseend && newcourseendmin>coursestartmin ) {
+                        isoverlap=true;
+                    } else if (newcourseend==courseend && newcourseendmin<courseendmin ) {
+                        isoverlap=true;
+                    } else if (newcoursestart==coursestart && newcoursestartmin==coursestartmin) {
+                        isoverlap=true;
+                    } else if (newcourseend==courseend && courseendmin==newcourseendmin) {
+                        isoverlap=true;
+                    }
+                }
+            }
+
         }
     }
 }
